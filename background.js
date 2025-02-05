f77fd089b4dd44e56efe2e1e0d1939d636e82ed3
Sen Harald Hjort Inao <senharald07@gmail.com>
2025-02-05 18:45:36 +0100

    added better cookie detection and stock market tracking

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-let gamestate = {nextTick: null}
+let gamestate
 let previousGamestate = gamestate
-let cookieTimer = 0
 let notifications = []
 
 function getPlotChanges(prevPlot, currPlot, plants) {
   let changes = []
   for (let y = 0;y<6;y++) {
     for (let x = 0;x<6;x++) {
+      let msg = ""
       let prevTile = prevPlot[y][x]
       let currTile = currPlot[y][x]
       if (!currTile[0] && !prevTile[0]) {
@@ -14,20 +14,33 @@ function getPlotChanges(prevPlot, currPlot, plants) {
       }
 
       if (prevTile[0] && !currTile[0]) {
-        changes.push("A plant has died")
-        continue
+        msg = `${plants[prevTile[0]-1].name} has died!`
       } else if (!prevTile[0] && currTile[0]) {
-        changes.push("A new plant has appeared")
-        continue
+        msg = `${plants[currTile[0]-1].name} has Appeared!`
       } else {
         let maturity = plants[prevTile[0]-1].mature
         if (prevTile[1] < maturity && currTile[1] >= maturity) {
-          changes.push("A plant has grown up!")
+          msg = `${plants[currTile[0]-1].name} has grown up!`
         }
+      }
+      if (msg) {
+        changes.push(msg)
       }
     }
   }
   return changes
+}
+
+function checkShimmerSpawn(prevShimmers, currShimmers) {
+  if (prevShimmers.length < currShimmers.length) {
+    return 1
+  } else if (!prevShimmers.length && !currShimmers.length) {
+    return 0
+  } else if (prevShimmers.length > currShimmers.length) {
+    return 0
+  }
+
+  return 0
 }
 
 function stateUpdater() {
@@ -39,26 +52,9 @@ function stateUpdater() {
           target: { tabId: cookieTab.id },
           func: getContent,
           world: 'MAIN'
-        },
-        (results) => {
+        }, (results) => {
           if (results?.[0]?.result) {
-            previousGamestate = gamestate
-            gamestate = results[0].result
-
-            if (previousGamestate.nextTick < gamestate.nextTick) {
-              notifications.push(...getPlotChanges(previousGamestate.plot, gamestate.plot, gamestate.plants))
-            }
-
-            if (gamestate.goldencookies && !cookieTimer) {
-              notifications.push("A Golden cookie has appeared")
-              cookieTimer = 1
-              setTimeout(()=>{cookieTimer=0}, 10000)
-            }
-
-            if (notifications.length) {
-              notifyUser(notifications.join("\n"))
-              notifications = []
-            }
+            handleGamestate(results[0].result)
           }
         }
       )
@@ -66,11 +62,43 @@ function stateUpdater() {
   })
 }
 
+function getMarketChanges(prevStocks, currStocks) {
+  let changes = []
+  Object.keys(currStocks).forEach((key)=>{
+    if (currStocks[key].active && currStocks[key].val < 2 && prevStocks[key].val >= 2) {
+      changes.push(`${currStocks[key].name} stock has gone below 2!`)
+    }
+  })
+  return changes
+}
+
+function handleGamestate(newgamestate) {
+  previousGamestate = gamestate
+  gamestate = newgamestate
+
+  if (previousGamestate.farmT < gamestate.farmT) {
+    notifications.push(...getPlotChanges(previousGamestate.plot, gamestate.plot, gamestate.plants))
+  }
+
+  if (!gamestate.buffs["Cookie storm"] && checkShimmerSpawn(previousGamestate.shimmers, gamestate.shimmers)) {
+    notifications.push("A golden cookie has appeared")
+  }
+
+  if (gamestate.bankT < previousGamestate.bankT) {
+    notifications.push(...getMarketChanges(previousGamestate.stocks, gamestate.stocks))
+  }
+
+  if (notifications.length) {
+    notifyUser(notifications.join("\n"))
+    notifications = []
+  }
+}
+
 function notifyUser(msg) {
   chrome.notifications.create({
     type: "basic",
     iconUrl: chrome.runtime.getURL("icon.png"),
-    title: "Cookie notification",
+    title: "Cookie clicker",
     message: msg,
     priority: 2
   });
@@ -87,13 +115,24 @@ function getContent() {
     return `${scaled.toFixed(3)} ${suffix}`
   }
 
-  return window.Game ? {
+  if (!window.Game) return null
+
+  let goods = Game.Objects["Bank"].minigame.goods
+  let stocks = {}
+  Object.keys(goods).forEach(key => {
+    stocks[key] = {val: goods[key].val, active: goods[key].active, name: goods[key].name}
+  })
+  
+  return {
     cookies: formatCookies(Game.cookies),
-    nextTick: (Game.Objects["Farm"].minigame.nextStep - Date.now()) / 1000,
+    farmT: (Game.Objects["Farm"].minigame.nextStep - Date.now()) / 1000,
     plot: Game.Objects["Farm"].minigame.plot,
     plants: Game.Objects["Farm"].minigame.plantsById,
-    goldencookies: Game.shimmers.length
-  } : null
+    shimmers: Game.shimmers,
+    buffs: Game.buffs,
+    stocks: stocks,
+    bankT: Game.Objects["Bank"].minigame.tickT/30
+  }
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -102,13 +141,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       sendResponse({active:true, game:gamestate})
     } else {
       sendResponse({active:false})
-    }
-  } else if (message.action === "getUpdates") {
-    if (notifications.length) {
-      sendResponse(notifications)
-      notifications = []
-    } else {
-      sendResponse(null)
     }
   }
 })
